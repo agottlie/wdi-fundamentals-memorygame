04c8f85ee74e5e6c5fd198739012d708ba03164a
Andrew Gottlieb <andrewgottlieb@Admins-MacBook-Air-3.local>
2017-05-08 16:15:21 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -21,11 +21,21 @@ var cards = [
 	},
 ];
 var cardsInPlay = [];
+var wins=0;
+var losses=0;
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
       	alert("You found a match!");
+      	wins+=1;
+      	document.getElementById('wins').textContent = 'Wins: ' + wins;
   	} else {
     	alert("Sorry, try again.");
+    	losses+=1;
+    	document.getElementById('losses').textContent = 'Losses: ' + losses;
+  	}
+  	cardsInPlay.length=0;
+  	for (i=0; i<cards.length; i++) {
+  		document.getElementsByTagName('img')[i].setAttribute('src', "images/back.png");
   	}
 }
 var flipCard = function() {
